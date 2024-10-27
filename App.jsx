8c8517d7813fe0e7e5f5a3dc185743163dca5f71
seliinatmaca <selinatmacayy@gmail.com>
2024-10-27 14:36:46 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/navigation/Routes';
+import {UserProvider} from './src/context/UserContext';
+import {TaskProvider} from './src/context/TaskContext';
+
+export default function App() {
+  return (
+    <UserProvider>
+      <TaskProvider>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </TaskProvider>
+    </UserProvider>
+  );
+}
---
+++ src/navigation/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import UserListScreen from '../screens/UserListScreen';
+import UserDetailsScreen from '../screens/UserDetailsScreen';
+import TaskScreen from '../screens/TaskScreen';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="UserList" component={UserListScreen} />
+      <Stack.Screen name="UserDetails" component={UserDetailsScreen} />
+      <Stack.Screen name="Tasks" component={TaskScreen} />
+    </Stack.Navigator>
+  );
+}
